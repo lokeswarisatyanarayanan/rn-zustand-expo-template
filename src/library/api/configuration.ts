63913cd329b37f64a1d27a5d527632a7c36a45ef
Lokeswari <lokeswari@newgendigital.com>
2025-07-14 01:29:38 +0530

    feat: updated Readme

--- src/library/api/configuration.ts
+++ src/library/api/configuration.ts
@@ -1,3 +1,4 @@
 import Constants from 'expo-constants';
 
-export const API_BASE_URL = Constants.expoConfig?.extra?.API_BASE_URL || '';
+export const API_BASE_URL =
+  Constants.expoConfig?.extra?.API_BASE_URL || 'https://jsonplaceholder.typicode.com';
